055e4e58edf60d7731cdc42441bffb907887be7c
AndresFMoya <andresfmoya@gmail.com>
2019-12-11 15:26:41 -0500

    chore: add function book and library array

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const library = [ 
+    { 
+        id: 1, 
+        title: 'Lord of The Ring', 
+        author: 'J. R. Tolkien', 
+        status: true, 
+    }, 
+];
+
+
+function Book(id, title, author, status) {
+    this.id = id
+    this.title = title
+    this.author = author
+    this.status = status
+}
+
