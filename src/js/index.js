09b90354842c65920370e50c5884f433d0a215b0
AndresFMoya <andresfmoya@gmail.com>
2019-12-11 16:05:28 -0500

    feat: add function addBooktoLibrary and render()

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
-const library = [ 
+const library = [
     { 
         id: 1, 
         title: 'Lord of The Ring', 
         author: 'J. R. Tolkien', 
+        pages: 488,
         status: true, 
     }, 
 ];
@@ -15,3 +16,25 @@ function Book(id, title, author, status) {
     this.status = status
 }
 
+function addBookToLibrary(book = library[0]) {
+    library.push(book)
+    render()
+}
+
+function render() {
+    let rows = '';
+    library.forEach((book) => {
+        rows +=
+        `<tr>
+            <td>${book.id}</td>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td>${book.status ? 'Readed' : 'Not Readed'}</td>
+        </tr>`
+        ;
+    });
+    document.getElementById('table-body').innerHTML = rows
+}
+
+render()
